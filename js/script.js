37613e5d8168c420645c59650a75a31313998bb9
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-03-15 17:04:03 +0530

    Project Complete

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const apiKey = "58b9914c13cf93af0f52d4cb17dd12ff"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric"
+const inputField = document.querySelector('input')
+
+const weatherImg = document.querySelectorAll('img')[0];
+
+async function checkWeather() {
+    let cityName = inputField.value
+    console.log(cityName)
+
+    const response = await fetch(apiUrl + `&q=${cityName}` + `&appid=${apiKey}`)
+    let data = await response.json()
+
+    console.log(data);
+
+    //Choose the elements
+    const humidity = document.querySelector('#humidity p')
+    const wind = document.querySelector("#windSpeed p")
+    const city = document.getElementById("city")
+    const temperature = document.getElementById("temp")
+
+    //Fill Out the element appropriately.
+    humidity.innerHTML = `${data.main.humidity}%`;
+    wind.innerHTML = `${data.wind.speed} km/h`;
+    city.innerHTML = `${data.name}`
+
+    let temp = Math.floor(parseInt(data.main.temp));
+
+    temperature.innerHTML = `${(temp)}&#176;C`
+
+    let main = data.weather[0].main;
+    console.log(data.weather[0].main);
+
+    if (main === 'Clouds') {
+        weatherImg.src = '../img/images/clouds.png'
+    } else if (main === 'Clear') {
+        weatherImg.src = '../img/images/clear.png'
+    } else if (main === 'Drizzle') {
+        weatherImg.src = "../img/images/drizzle.png"
+    } else if (main === 'Mist') {
+        weatherImg.src = "../img/images/mist.png"
+    } else if (main === 'Rain') {
+        weatherImg.src = "../img/images/rain.png"
+    }
+
+    inputField.value = "";
+}
+
+const btn = document.getElementById('searchBtn')
+
+btn.addEventListener('click', checkWeather)
+
+inputField.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        checkWeather();
+    }
+})
